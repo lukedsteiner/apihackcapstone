530b5a6a8539647ef43f34aba9f503dea39a3180
Luke Steiner <luke.steiner@bigcommerce.com>
2019-09-30 17:27:21 -0500

    fixed calculator, added a few stories

--- script.js
+++ script.js
@@ -2,46 +2,65 @@
 
 function calculateSunsign(value) {
   console.log(value);
-  typeof value;
+  let sunsign = 'placeholder';
   if (value <= 2.18 && value >= 1.20) {
-    const sunsign = 'Aquarius';
+    sunsign = 'Aquarius';
   }
   else if (value <= 3.2 && value >= 2.19) {
-    let sunsign = 'Pisces';
+    sunsign = 'Pisces';
   }
   else if (value <= 4.19 && value >= 3.21) {
-    let sunsign = 'Aries';
+    sunsign = 'Aries';
   }
   else if (value <= 5.2 && value >= 4.2) {
-    let sunsign = 'Taurus';
+    sunsign = 'Taurus';
   }
   else if (value <= 6.2 && value >= 5.21) {
-    let sunsign = 'Gemini';
+    sunsign = 'Gemini';
   }
   else if (value <= 7.22 && value >= 6.21) {
-    let sunsign = 'Cancer';
+    sunsign = 'Cancer';
   }
   else if (value <= 8.22 && value >= 7.23) {
-    let sunsign = 'Leo'; 
+    sunsign = 'Leo'; 
   }
   else if (value <= 9.22 && value >= 8.23) {
-    let sunsign = 'Virgo';
+    sunsign = 'Virgo';
   }
   else if (value <= 10.22 && value >= 9.23) {
-    let sunsign = 'Libra';
+    sunsign = 'Libra';
   }
   else if (value <= 11.21 && value >= 10.23) {
-    let sunsign = 'Scorpio';
+    sunsign = 'Scorpio';
   }
   else if (value <= 12.21 && value >= 11.22) {
-    let sunsign = 'Sagittarius';
+    sunsign = 'Sagittarius';
   }
   else {
-    let sunsign = 'Capricorn';
+    sunsign = 'Capricorn';
   }
 
-  console.log(sunsign);
-  // nextForm(sunsign)
+  console.log(`${sunsign}`);
+  nextForm(sunsign);
+}
+
+function watchFormTwo() {
+  $('.js-horoscope').submit(event => {
+    event.preventDefault();
+    const sunsign = $('#js-sunsign').val();
+    const day = $('#js-day').val();
+    getHoroscope(sunsign, day);
+  })
+}
+
+ function nextForm(sunsign) {
+  This will create the content to show the next form, with sunsign, day, and submit PushSubscriptionOptions. 
+}
+
+function skipFormOne() {
+  This will run if the skip button is chosen, rather than the submit button. 
+  The page will reload ; 
+  nextForm();
 }
 
 function watchFormOne() {
