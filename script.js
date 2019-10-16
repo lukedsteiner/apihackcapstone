e9e9820c108493426e57bb389eacc8b9ca03847c
Luke Steiner <luke.steiner@bigcommerce.com>
2019-10-16 17:37:22 -0500

    removed images, edit forms to be more simple, fixed height/width issues on logo

--- script.js
+++ script.js
@@ -14,61 +14,47 @@ function createContent(compatibility, mood, lucky, description) {
   <section class="mood">${mood}</section>
   <h3>Lucky Number</h3>
   <section class="lucky number">${lucky}</section>
-  <button type="button" id="restartButton">New Sign or Day?</button>
   <button type="button" id="clearButton">Restart?</button>`
   );
-  watchRestart();
   watchClear();
 }
 
 function calculateSunsign(value) {
   if (value <= 2.18 && value >= 1.20) {
-    sunsign = 'aquarius';
-    symbol = 'https://www.astrology-zodiac-signs.com/images/aquarius.jpg'
+    sunsign = 'Aquarius';
   }
   else if (value <= 3.2 && value >= 2.19) {
-    sunsign = 'pisces';
-    symbol = 'https://www.astrology-zodiac-signs.com/images/pisces.jpg'
+    sunsign = 'Pisces';
   }
   else if (value <= 4.19 && value >= 3.21) {
-    sunsign = 'aries';
-    symbol = 'https://www.astrology-zodiac-signs.com/images/aries.jpg'
+    sunsign = 'Aries';
   }
   else if (value <= 5.2 && value >= 4.2) {
-    sunsign = 'taurus';
-    symbol = 'https://www.astrology-zodiac-signs.com/images/taurus.jpg'
+    sunsign = 'Taurus';
   }
   else if (value <= 6.2 && value >= 5.21) {
-    sunsign = 'gemini';
-    symbol = 'https://www.astrology-zodiac-signs.com/images/gemini.jpg'
+    sunsign = 'Gemini';
   }
   else if (value <= 7.22 && value >= 6.21) {
-    sunsign = 'cancer';
-    symbol ='https://www.astrology-zodiac-signs.com/images/cancer.jpg'
+    sunsign = 'Cancer';
   }
   else if (value <= 8.22 && value >= 7.23) {
-    sunsign = 'leo'; 
-    symbol = 'https://www.astrology-zodiac-signs.com/images/leo.jpg'
+    sunsign = 'Leo'; 
   }
   else if (value <= 9.22 && value >= 8.23) {
-    sunsign = 'virgo';
-    symbol = 'https://www.astrology-zodiac-signs.com/images/virgo.jpg'
+    sunsign = 'Virgo';
   }
   else if (value <= 10.22 && value >= 9.23) {
-    sunsign = 'libra';
-    symbol = 'https://www.astrology-zodiac-signs.com/images/libra.jpg'
+    sunsign = 'Libra';
   }
   else if (value <= 11.21 && value >= 10.23) {
-    sunsign = 'scorpio';
-    symbol = 'https://www.astrology-zodiac-signs.com/images/scorpio.jpg'
+    sunsign = 'Scorpio';
   }
   else if (value <= 12.21 && value >= 11.22) {
-    sunsign = 'sagittarius';
-    symbol = 'https://www.astrology-zodiac-signs.com/images/sagittarius.jpg'
+    sunsign = 'Sagittarius';
   }
   else {
-    sunsign = 'capricorn';
-    symbol = 'https://www.astrology-zodiac-signs.com/images/capricorn.jpg'
+    sunsign = 'Capricorn';
   }
 
   nextForm(sunsign);
@@ -146,7 +132,6 @@ function watchClear() {
                  <option value = "31">31</option>
          </select>
          <input type="submit" value="Submit">
-         <input type="button" id="skipButton" value="Skip">
          </p>
        </form>
        </fieldset>`);
@@ -154,46 +139,6 @@ function watchClear() {
        $(`#js-month option[value="${month}"]`).attr("selected",true);
     watchFormOne();
 })}
-
-function watchRestart() {
-  $('.content-container').on('click', '#restartButton', function(event) {
-    $('.content-container').empty();
-   $('.content-container').append(
-     `    <form class="js-horoscope">
-     <fieldset>
-     <legend>Choose your Sunsign, and pick which day's horoscope you would like!</legend>
-     <p>
-     <select id = "js-sunsign">
-             <option value = "aquarius">Aquarius</option>
-             <option value = "pisces">Pisces</option>
-             <option value = "aries">Aries</option>
-             <option value = "taurus">Taurus</option>
-             <option value = "gemini">Gemini</option>
-             <option value = "cancer">Cancer</option>
-             <option value = "leo">Leo</option>
-             <option value = "virgo">Virgo</option>
-             <option value = "libra">Libra</option>
-             <option value = "scorpio">Scorpio</option>
-             <option value = "sagittarius">Sagittarius</option>
-             <option value = "capricorn">Capricorn</option>
-     </select>
-     <select id = "js-query">
-             <option value = "yesterday">Yesterday</option>
-             <option value = "today">Today</option>
-             <option value = "tomorrow">Tomorrow</option>
-
-     </select>
-     <input type="submit" value="Submit">
-     </p>
-   </form>
-   </fieldset>
-   <p id="js-error-message" class="error-message"></p>`
-   )
-  $(`#js-sunsign option[value="${sunsign}"]`).attr("selected",true);
-  $('#js-query option[value="today"]').attr("selected",true);
-  watchFormTwo();
-})
-}
 
 function watchFormTwo() {
   $('.js-horoscope').submit(event => {
@@ -208,24 +153,23 @@ function watchFormTwo() {
    $('.content-container').empty();
    $('.content-container').append(
      `<section class='sign-indicator'>The date (if selected) is within the range of the ${sunsign} sign</section>
-     <img src='${symbol}' alt="zodiac icon">
      <form class="js-horoscope">
      <fieldset>
      <legend>Choose your Sunsign, and pick which day's horoscope you would like!</legend>
      <p>
      <select id = "js-sunsign">
-             <option value = "aquarius">Aquarius</option>
-             <option value = "pisces">Pisces</option>
-             <option value = "aries">Aries</option>
-             <option value = "taurus">Taurus</option>
-             <option value = "gemini">Gemini</option>
-             <option value = "cancer">Cancer</option>
-             <option value = "leo">Leo</option>
-             <option value = "virgo">Virgo</option>
-             <option value = "libra">Libra</option>
-             <option value = "scorpio">Scorpio</option>
-             <option value = "sagittarius">Sagittarius</option>
-             <option value = "capricorn">Capricorn</option>
+             <option value = "Aquarius">Aquarius</option>
+             <option value = "Pisces">Pisces</option>
+             <option value = "Aries">Aries</option>
+             <option value = "Taurus">Taurus</option>
+             <option value = "Gemini">Gemini</option>
+             <option value = "Cancer">Cancer</option>
+             <option value = "Leo">Leo</option>
+             <option value = "Virgo">Virgo</option>
+             <option value = "Libra">Libra</option>
+             <option value = "Scorpio">Scorpio</option>
+             <option value = "Sagittarius">Sagittarius</option>
+             <option value = "Capricorn">Capricorn</option>
      </select>
      <select id = "js-query">
              <option value = "yesterday">Yesterday</option>
@@ -244,11 +188,6 @@ function watchFormTwo() {
   watchFormTwo();
 }
 
-function skipFormOne() {
-  $('.js-birth-date').on('click', '#skipButton', function(event) {
-  nextForm('aquarius');
-  });
-};
 
 
 function watchFormOne() {
